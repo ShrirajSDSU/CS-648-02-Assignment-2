cbe96421707b8fa917f8fbad354e1a8ecec1a026
ShrirajSDSU <89901228+ShrirajSDSU@users.noreply.github.com>
2022-02-17 09:45:14 -0800

    updated port number

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 app.use(express.static('public'));
 
-app.listen(3002, function() {
+app.listen(3000, function() {
     console.log('App running on port 3000')
 });
